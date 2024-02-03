218663854a3cb453f9062fc8e3c9fd4cd27ced5a
Ankit Singh <thakurbharat065@gmail.com>
2024-02-03 19:36:17 +0530

    chnages

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const cors = require("cors");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
 
 // Using Middlewares
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
